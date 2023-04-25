7930af8745d315623f8ce4b53a49ae689190fb85
sunaoka <sunaoka@pocari.org>
2023-04-25 18:13:29 +0900

    Add PHPStan

--- src/Eloquent/Model.php
+++ src/Eloquent/Model.php
@@ -8,6 +8,11 @@ class Model extends \Illuminate\Database\Eloquent\Model
 {
     use Concerns\HasAttributes;
 
+    /**
+     * @param \Sunaoka\LaravelPostgres\Query\Builder $query
+     *
+     * @return \Sunaoka\LaravelPostgres\Eloquent\Builder<\Sunaoka\LaravelPostgres\Eloquent\Model>
+     */
     public function newEloquentBuilder($query)
     {
         return new Builder($query);
