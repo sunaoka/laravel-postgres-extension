17c9d5cf943fd5c5cca11abe42756fd1271058d2
sunaoka <sunaoka@pocari.org>
2024-12-11 14:54:33 +0900

    Fixed namespace

--- tests/Models/ModelTest.php
+++ tests/Models/ModelTest.php
@@ -2,18 +2,18 @@
 
 declare(strict_types=1);
 
-namespace Sunaoka\LaravelPostgres\Tests\Query;
+namespace Sunaoka\LaravelPostgres\Tests\Models;
 
-use Sunaoka\LaravelPostgres\Tests\Models\TestModel;
+use Sunaoka\LaravelPostgres\Eloquent\Builder;
 use Sunaoka\LaravelPostgres\Tests\TestCase;
 
 class ModelTest extends TestCase
 {
-    public function testNewEloquentBuilder(): void
+    public function test_new_eloquent_builder(): void
     {
         $model = new TestModel;
         $actual = $model->newEloquentBuilder($this->getQueryBuilder());
 
-        self::assertInstanceOf(\Sunaoka\LaravelPostgres\Eloquent\Builder::class, $actual);
+        self::assertInstanceOf(Builder::class, $actual);
     }
 }
